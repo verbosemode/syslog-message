eead92c4521cf5e15bfefbf4aa4230a64d4fb2c9
Marek Kubica <marek@xivilization.net>
2016-03-14 15:24:21 +0100

    Add first test to make sure it fails properly

--- src/test_syslog_message.ml
+++ src/test_syslog_message.ml
@@ -1,3 +1,14 @@
+module Smsg = Syslog_message
+
+let invalid_data_fails = QCheck.mk_test ~n:100
+  ~name:"Check that putting in invalid data always fails"
+  QCheck.Arbitrary.(string)
+  @@ fun msg ->
+    let ctx = {Smsg.timestamp=Ptime.epoch;
+               Smsg.hostname="";
+               Smsg.set_hostname=false} in
+    Smsg.parse ctx msg = None
+
 let () =
-  let suite = [] in
+  let suite = [invalid_data_fails] in
   ignore @@ QCheck.run_tests suite
